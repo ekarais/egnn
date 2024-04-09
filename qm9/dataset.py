4aa82f564d2c3a77bbf644a851da348cdfa2095c
Ege Karaismailoglu <ekaraismailoglu@holygpu7c26306.rc.fas.harvard.edu>
2024-04-09 14:34:22 -0400

    black + isort

--- qm9/dataset.py
+++ qm9/dataset.py
@@ -1,33 +1,44 @@
+import torch
 from torch.utils.data import DataLoader
-from qm9.data.utils import initialize_datasets
+
 from qm9.args import init_argparse
 from qm9.data.collate import collate_fn
-import torch
+from qm9.data.utils import initialize_datasets
+
 
 def retrieve_dataloaders(batch_size, num_workers=1):
     # Initialize dataloader
-    args = init_argparse('qm9')
-    args, datasets, num_species, charge_scale = initialize_datasets(args, args.datadir, 'qm9',
-                                                                    subtract_thermo=args.subtract_thermo,
-                                                                    force_download=args.force_download
-                                                                    )
-    qm9_to_eV = {'U0': 27.2114, 'U': 27.2114, 'G': 27.2114, 'H': 27.2114, 'zpve': 27211.4, 'gap': 27.2114, 'homo': 27.2114,
-                 'lumo': 27.2114}
+    args = init_argparse("qm9")
+    args, datasets, num_species, charge_scale = initialize_datasets(
+        args, args.datadir, "qm9", subtract_thermo=args.subtract_thermo, force_download=True
+    )
+    qm9_to_eV = {
+        "U0": 27.2114,
+        "U": 27.2114,
+        "G": 27.2114,
+        "H": 27.2114,
+        "zpve": 27211.4,
+        "gap": 27.2114,
+        "homo": 27.2114,
+        "lumo": 27.2114,
+    }
 
     for dataset in datasets.values():
         dataset.convert_units(qm9_to_eV)
 
-
     # Construct PyTorch dataloaders from datasets
-    dataloaders = {split: DataLoader(dataset,
-                                     batch_size=batch_size,
-                                     shuffle=args.shuffle if (split == 'train') else False,
-                                     num_workers=num_workers,
-                                     collate_fn=collate_fn)
-                         for split, dataset in datasets.items()}
+    dataloaders = {
+        split: DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=args.shuffle if (split == "train") else False,
+            num_workers=num_workers,
+            collate_fn=collate_fn,
+        )
+        for split, dataset in datasets.items()
+    }
 
     return dataloaders, charge_scale
-
 
 
 def batch_stack(props):
@@ -84,9 +95,9 @@ def drop_zeros(props, to_keep):
         return props[:, to_keep, ...]
 
 
-if __name__ == '__main__':
-    '''
+if __name__ == "__main__":
+    """
     dataloader = retrieve_dataloaders(batch_size=64)
     for i, batch in enumerate(dataloader['train']):
         print(i)
-    '''
+    """
